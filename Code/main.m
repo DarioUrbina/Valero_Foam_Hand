4a2d6dd7afb46ad9b16cc367508b8aff2ac574e3
Darío Urbina <urbiname@usc.edu>
2021-04-19 12:04:56 -0700

    prediction process done successfully

--- Code/main.m
+++ Code/main.m
@@ -27,23 +27,23 @@ donwsampling_rate = 50;                                                    % the
 downsampled_data = downsample(data(25:end,:),donwsampling_rate);           % downsampling the data
 donwsampled_and_labeled_data =...
     zeros(floor(size(data,1)/donwsampling_rate), size(data,2)+1);          % pre-alocating donwsampled_and_labeled_data
-repeated_labels=repmat(1:9,1,ceil(size(downsampled_data,1)/9));            % generaqting labels
+repeated_labels=repmat(1:6,1,ceil(size(downsampled_data,1)/6));            % generaqting labels
 donwsampled_and_labeled_data(:,1:end-1)=downsampled_data;                  % downsampling the data
 labels=repeated_labels(1:size(downsampled_data,1));                        % selecting labels consistent to the lenght of the recording
 donwsampled_and_labeled_data(:,end)=labels;                                % assigning the labels to the downsampled data
 %% plotting and savingthe processed data
-figure(); plot(donwsampled_and_labeled_data(:,1:4))                        % plotting sensor readings
+figure(); plot(donwsampled_and_labeled_data(:,1:6))                        % plotting sensor readings
 legend(...
-    'Sensor 1','Sensor 2','Sensor 3','Sensor 4', 'location', 'southeast')  % setting the plot legend
-csvwrite(data,donwsampled_and_labeled_data)                           % writing the preprocessed data into a csv file
+    'Sensor 1','Sensor 2','Sensor 3','Sensor 4','Sensor 5','Sensor 6', 'location', 'southeast')  % setting the plot legend
+csvwrite('donwsampled_and_labeled_data',donwsampled_and_labeled_data)                           % writing the preprocessed data into a csv file
 %% KNN classification
 rng(99)                                                                   % setting the random seed to a constant to have consistencu across results; can do a cross-validation for the final results
 K=2;                                                                       % setting K in KNN; number of nearest neighbors to be participated in the voting
 test_percentage=.8;                                                        % setting the test/train ratio
 [train_data,test_data] = dividerand(donwsampled_and_labeled_data',...
     test_percentage, 1-test_percentage);                                   % randomized test, train split
-mdl=fitcknn(train_data(1:4,:)',train_data(5,:)','NumNeighbors',K);         % fitting the KNN model
-KNNPred=predict(mdl,test_data(1:4,:)');                                    % running the KNN model on the test data
+mdl=fitcknn(train_data(1:6,:)',train_data(7,:)','NumNeighbors',K);         % fitting the KNN model
+KNNPred=predict(mdl,test_data(1:6,:)');                                    % running the KNN model on the test data
 accuracy=sum((KNNPred-test_data(end,:)')==0)/length(KNNPred);              % calculating the accuracy on the test set
 disp(['accuracy is ', num2str(100*accuracy), '%'])                         % displaying accuracy percentage on the Command Window
 %% plotting classification results
