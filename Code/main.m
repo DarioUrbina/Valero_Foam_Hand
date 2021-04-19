feb9b45e57e317c93e80fd23263318ec2d9fe886
Darío Urbina <urbiname@usc.edu>
2021-04-19 12:36:21 -0700

    Generalizing code: Variables created to facilitate changing number of labels and sensors

--- Code/main.m
+++ Code/main.m
@@ -1,38 +1,46 @@
 %% initialization
 clear all;close all;
 %clc; %#ok<CLALL>                                                          % cleaning the Workspace and Command Windows
-file_name_1='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_1';     % the data file name
-data_1 = importdata(strcat(file_name_1,'.txt'));                           
-file_name_2='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_2'; 
-data_2 = importdata(strcat(file_name_2,'.txt'));                           
-file_name_3='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_3'; 
-data_3 = importdata(strcat(file_name_3,'.txt'));                           
-file_name_4='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_4'; 
-data_4 = importdata(strcat(file_name_4,'.txt'));                           
-file_name_5='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_5'; 
-data_5 = importdata(strcat(file_name_5,'.txt'));                           
-file_name_6='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_6'; 
-data_6 = importdata(strcat(file_name_6,'.txt'));                          
-file_name_7='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_7'; 
-data_7 = importdata(strcat(file_name_7,'.txt'));                          
-file_name_8='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_8'; 
-data_8 = importdata(strcat(file_name_8,'.txt'));                            
-file_name_9='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_9'; 
-data_9 = importdata(strcat(file_name_9,'.txt'));                            
-file_name_10='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_10'; 
-data_10 = importdata(strcat(file_name_10,'.txt'));                          
 
-data = [data_1;data_2;data_3;data_4;data_5;data_6;data_7;data_8;data_9;data_10];
+number_of_labels = 5 ;
+number_of_sensors = 10 ;
+
+file_name_1='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_10sensors_0411\test_hand_single_0411_1';     % the data file name
+data = importdata(strcat(file_name_1,'.txt'));                           
+% 
+% file_name_1='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_1';
+% data = importdata(strcat(file_name_1,'.txt'));                           
+% file_name_2='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_2'; 
+% data_2 = importdata(strcat(file_name_2,'.txt'));                           
+% file_name_3='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_3'; 
+% data_3 = importdata(strcat(file_name_3,'.txt'));                           
+% file_name_4='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_4'; 
+% data_4 = importdata(strcat(file_name_4,'.txt'));                           
+% file_name_5='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_5'; 
+% data_5 = importdata(strcat(file_name_5,'.txt'));                           
+% file_name_6='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_6'; 
+% data_6 = importdata(strcat(file_name_6,'.txt'));                          
+% file_name_7='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_7'; 
+% data_7 = importdata(strcat(file_name_7,'.txt'));                          
+% file_name_8='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_8'; 
+% data_8 = importdata(strcat(file_name_8,'.txt'));                            
+% file_name_9='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_9'; 
+% data_9 = importdata(strcat(file_name_9,'.txt'));                            
+% file_name_10='C:\Users\dario\Documents\Github\Valero_Foam_Hand\Data\Data_6sensors_0410\test_hand_single_0410_10'; 
+% data_10 = importdata(strcat(file_name_10,'.txt'));                          
+% 
+% data = [data;data_2;data_3;data_4;data_5;data_6;data_7;data_8;data_9;data_10];
+
 donwsampling_rate = 50;                                                    % the downsampling rate
-downsampled_data = downsample(data(25:end,:),donwsampling_rate);           % downsampling the data
+downsampled_data = downsample(data(25:end,:),donwsampling_rate);           % downsampling the data   
 donwsampled_and_labeled_data =...
     zeros(floor(size(data,1)/donwsampling_rate), size(data,2)+1);          % pre-alocating donwsampled_and_labeled_data
-repeated_labels=repmat(1:6,1,ceil(size(downsampled_data,1)/6));            % generaqting labels
+repeated_labels=repmat(1:number_of_labels,1,ceil(size(downsampled_data,1)/number_of_labels));            % generaqting labels
 donwsampled_and_labeled_data(:,1:end-1)=downsampled_data;                  % downsampling the data
 labels=repeated_labels(1:size(downsampled_data,1));                        % selecting labels consistent to the lenght of the recording
 donwsampled_and_labeled_data(:,end)=labels;                                % assigning the labels to the downsampled data
 %% plotting and savingthe processed data
-figure(); plot(donwsampled_and_labeled_data(:,1:6))                        % plotting sensor readings
+figure(); plot(donwsampled_and_labeled_data(:,1:number_of_labels))                        % plotting sensor readings
 legend(...
     'Sensor 1','Sensor 2','Sensor 3','Sensor 4','Sensor 5','Sensor 6', 'location', 'southeast')  % setting the plot legend
 csvwrite('donwsampled_and_labeled_data',donwsampled_and_labeled_data)                           % writing the preprocessed data into a csv file
@@ -42,8 +50,8 @@ K=2;                                                                       % set
 test_percentage=.8;                                                        % setting the test/train ratio
 [train_data,test_data] = dividerand(donwsampled_and_labeled_data',...
     test_percentage, 1-test_percentage);                                   % randomized test, train split
-mdl=fitcknn(train_data(1:6,:)',train_data(7,:)','NumNeighbors',K);         % fitting the KNN model
-KNNPred=predict(mdl,test_data(1:6,:)');                                    % running the KNN model on the test data
+mdl=fitcknn(train_data(1:number_of_sensors,:)',train_data(number_of_sensors+1,:)','NumNeighbors',K);         % fitting the KNN model
+KNNPred=predict(mdl,test_data(1:number_of_sensors,:)');                                    % running the KNN model on the test data
 accuracy=sum((KNNPred-test_data(end,:)')==0)/length(KNNPred);              % calculating the accuracy on the test set
 disp(['accuracy is ', num2str(100*accuracy), '%'])                         % displaying accuracy percentage on the Command Window
 %% plotting classification results
